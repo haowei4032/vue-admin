d96a80a6ddd25a9e626bb59417356da9245388a7
haowei4032 <boss@haowei.me>
2018-11-24 09:44:36 +0800

    Merge branch 'master'

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -15,7 +15,7 @@ export default {
     methods: {
         logout() {
             sessionStorage.removeItem('name')
-            this.$router.push({path: '/login', query: { redirect: this.$route.path }})
+            this.$router.push({path: '/login', query: {redirect: this.$route.path}})
         }
     }
 }
